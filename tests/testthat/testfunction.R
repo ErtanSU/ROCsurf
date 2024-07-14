1a05bf3e8f188467fd73651f8348abd4887eaa66
ErtanSU <statistician.ertan@gmail.com>
2024-07-14 16:08:04 +0300

    nn1

--- tests/testthat/testfunction.R
+++ tests/testthat/testfunction.R
@@ -3424,16 +3424,124 @@ test_that("function return a double or list",
                        model=c("GLL"), method=c("WLSE")), "double")
 
 ###WGW
+  alpha1 <- 2; beta1 <- 1   # Weibull
+  alpha2 <- 2; beta2 <- 2  # Gamma
+  alpha3 <- 6; beta3 <- 9# Weibull
+  x<- rW(100,  alpha1,  beta1)
+  y <- rG(100,  alpha2, beta2)
+  z <- rW(100,  alpha3,  beta3)
 
+
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WGW"), method=c("MLE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WGW"), method=c("AD")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+            init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WGW"), method=c("CvM")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+             init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WGW"), method=c("LSE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+            init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WGW"), method=c("WLSE")), "double")
 
 ###WWW
+  alpha1 <- 1.5; beta1 <- 1   # Weibull
+  alpha2 <- 2; beta2 <- 1.5 # Weibull
+  alpha3 <- 3; beta3 <- 2   # Weibull
+  x<- rW(100,  alpha1,  beta1)
+  y <- rW(100,  alpha2, beta2)
+  z <- rW(100,  alpha3,  beta3)
 
+
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WWW"), method=c("MLE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+         init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WWW"), method=c("AD")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+          init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WWW"), method=c("CvM")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WWW"), method=c("LSE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+             init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("WWW"), method=c("WLSE")), "double")
 
 ###GGG
+  alpha1 <- 2; beta1 <- 2  # Gamma
+  alpha2 <- 3; beta2 <- 2 # Gamma
+  alpha3 <- 4; beta3 <- 3   # Gamma
+  x<- rG(100,  alpha1,  beta1)
+  y <- rG(100,  alpha2, beta2)
+  z <- rG(100,  alpha3,  beta3)
 
+
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+             init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("GGG"), method=c("MLE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("GGG"), method=c("AD")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+           init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("GGG"), method=c("CvM")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+          init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("GGG"), method=c("LSE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+           init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("GGG"), method=c("WLSE")), "double")
 
 ###LLL
+  alpha1 <- 1; beta1 <- 1   # Logistic
+  alpha2 <- 5; beta2 <- 1 # Logistic
+  alpha3 <- 7; beta3 <- 1   # Logistic
+  x<- rL(100,  alpha1,  beta1)
+  y <- rL(100,  alpha2, beta2)
+  z <- rL(100,  alpha3,  beta3)
 
+
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+               init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("LLL"), method=c("MLE")), "double")
+  # expect_type(r.tc_vus(x=x,y=y,z=z,
+  #             init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+  #                                   alpha3=alpha3,beta3=beta3),
+  #                      model=c("LLL"), method=c("AD")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+           init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("LLL"), method=c("CvM")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+          init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("LLL"), method=c("LSE")), "double")
+  expect_type(r.tc_vus(x=x,y=y,z=z,
+           init_param=c(alpha1=alpha1,beta1=beta1,alpha2=alpha2,beta2=beta2,
+                                    alpha3=alpha3,beta3=beta3),
+                       model=c("LLL"), method=c("WLSE")), "double")
 
 
  }
